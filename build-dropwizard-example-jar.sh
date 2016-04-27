1ec7f1ea27067add8eee1e3c931fc930db01e898
kimmowikman <kimmo.wikman@avarko.com>
2016-04-27 23:26:46 +0300

    Add mariadb client to dependencies.

--- build-dropwizard-example-jar.sh
+++ build-dropwizard-example-jar.sh
@@ -6,6 +6,7 @@ git config core.sparsecheckout true
 echo "dropwizard-example/*" >> .git/info/sparse-checkout
 git pull --depth=1 origin master
 cd dropwizard-example
+awk '/<dependencies>/ && ++n == 2{ print; print "<dependency>\n<groupId>org.mariadb.jdbc</groupId>\n<artifactId>mariadb-java-client</artifactId>\n<version>1.4.3</version>\n</dependency>"; next }1' pom.xml > pom.tmp && mv pom.tmp pom.xml
 mvn package
 cd ../..
 mkdir target
