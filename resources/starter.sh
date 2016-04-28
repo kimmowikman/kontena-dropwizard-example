2b8cc584a31346f80112a89df2e4da1fa8822ab4
kimmowikman <kimmo.wikman@avarko.com>
2016-04-28 23:32:59 +0300

    Fix db host paramter.

--- resources/starter.sh
+++ resources/starter.sh
@@ -1,5 +1,5 @@
 echo running starter.sh
-mysql -h$DWSERVER_DB_HOST-galera-ha.kontena.local -uroot -psecretpassword --execute "create database $DWSERVER_DB_NAME"
+mysql -h$DWSERVER_DB_HOST -uroot -psecretpassword --execute "create database $DWSERVER_DB_NAME"
 java -jar dropwizard-example.jar db migrate example-server.yml
 java -jar dropwizard-example.jar server example-server.yml
 
